bde88b38b6d37325313ed4634e542fbe44a5a3b2
vienhoanglong <Vienhoanglong789@gmail.com>
2022-01-28 20:20:27 +0700

    More info/Modal

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ picker.on('emoji', emoji => {
 button.addEventListener('click', () => {
   picker.togglePicker(button);
 });
+$('#search_chats').click(function(){
+  $('#search-chats').hasClass('d-none') ? $('#search-chats').removeClass('d-none') : $('#search-chats').addClass('d-none');
+})
+$('#close-search').click(function(){
+  $('#search-chats').addClass('d-none');
+})
+$('.btn-information').on('click', function(){
+  $('#more-option-area').toggleClass('col-3');
+  $('#more-option-area').toggleClass('d-none');
+});
